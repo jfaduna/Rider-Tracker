25d9b505064b60938d37ea4bbe2debd3f5934139
jaduna <jaduna@everesttech.com>
2026-02-19 07:44:21 +0800

    add: added debugger for queries

--- rider_tracker/rider_tracker/settings.py
+++ rider_tracker/rider_tracker/settings.py
@@ -137,3 +137,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "db_file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "debug.log",  # creates debug.log in project root
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["db_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
