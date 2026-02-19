fac508fbd3acbebce8d9deff89ce87338ddfe289
jaduna <jaduna@everesttech.com>
2026-02-19 16:53:26 +0800

    modify: increased token expiration time

--- rider_tracker/rider_tracker/settings.py
+++ rider_tracker/rider_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -155,4 +156,9 @@ LOGGING = {
             "propagate": False,
         },
     },
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(hours=4),
 }
